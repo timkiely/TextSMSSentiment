d6b55ed0e0b8eb61638bf72a68f09550a40fc7d1
Tim Kiely <timothy.j.kiely@gmail.com>
2014-05-30 19:59:29 -0400

    Finished first plot, added flow for in/out msgs

--- TextSentiment.R
+++ TextSentiment.R
@@ -1,3 +1,8 @@
+
+#Set messages to profile. Accepts character strings: "in" "out" or "all"
+MSGS<-"all"
+
+
 ##This script takes standard SMS output and attaches a sentiment scores to each text message
 ##Use the App "SMS To Text", found in the Google Play store to convert SMS to text
 ##The primary sentiment file used in v.1 is the AFINN.txt file found here: http://www2.imm.dtu.dk/pubdb/views/publication_details.php?id=6010
@@ -7,29 +12,36 @@ require(wordcloud)||{install.packages("wordcloud", dependencies = c("Depends","S
 require(tau)||{install.packages("tau", dependencies = c("Depends","Suggests"))} 
 require(dplyr)||{install.packages("dplyr", dependencies = c("Depends","Suggests"))} 
 
-#Reading in AFINN file
+##Reading in AFINN file and processing
 AFINN<-read.table("C:/R/AFINN-111.txt", sep="\t", stringsAsFactors=F)
 AFINN[1,1]<-as.character("abandon")
-
-#Processing the AFINN file to double effects of negative words. 
-#Can add controls to increase positive/negative effect
 AFINN<-rbind(AFINN[AFINN[,2]>=0,],
-             cbind(AFINN[AFINN[,2]<0,1],AFINN[AFINN[,2]<0,2]*2))
+             cbind(AFINN[AFINN[,2]<0,1],AFINN[AFINN[,2]<0,2]*2)) #doubling effect of negative words
 
 #Reading in the SMS file. Have included a few different options, for testing
 aFile <- readLines("C:/R/catsms_20131212.txt")
 #aFile <- readLines("C:/R/testIggysms_20131212.txt")
 #aFile <- readLines("C:/R/luposms_20131218.txt")
 
-#parse the text file according to SMS format into a dataframe
+##parse the text file
 aDf <- t(as.data.frame(strsplit(aFile,"\t")))
 colnames(aDf) <- c("Date","Time","Type","Num","Name","MSG")
 
-#subset by "in" msgs, if necessary
-aDf <- aDf[aDf[,"Type"]=="in",]
-cloudFile <- data.frame(MSG=as.character(aDf[,6]))
+##by setting MSGS above, subset by message types "in", "out" or "all"
+if(MSGS=="in"){
+  aDf <- aDf[aDf[,"Type"]=="in",]
+  } else {
+    if(MSGS=="out"){
+      aDf <- aDf[aDf[,"Type"]=="out",]
+    } else {
+      if(MSGS=="all"){
+        aDf <-aDf
+      }
+    }
+  }
 
-#convert each SMS to a document-term frequency matrix
+##Scoring each SMS
+##convert each SMS to a document-term frequency matrix
 fin<-data.frame()
 for(i in 1:nrow(aDf)){ #for each SMS
   options(warn=-1)
@@ -43,7 +55,7 @@ for(i in 1:nrow(aDf)){ #for each SMS
   if(is.null(DocumentTermMatrix(mCorpus)$dimnames$Terms)){ #flow control for SMS's with no scoreable words
     Score<-0
     } else {
-      mDTM <- DocumentTermMatrix(mCorpus) #create dTf Matrix
+      mDTM <- DocumentTermMatrix(mCorpus) #create dfTf Matrix
       m2 <- t(as.matrix(mDTM))
       m2df <- data.frame(as.vector(dimnames(m2)[1]),m2)
     
@@ -56,55 +68,60 @@ for(i in 1:nrow(aDf)){ #for each SMS
   fin<-rbind(fin,out)
 }
 
-#Create output table: original SMS dataframe with sentiment scores for each SMS  
-fin.df<-data.frame(aDf,fin)
-names(fin.df)<-c(colnames(aDf),"AllWordCount","Score")
+##Create output table: bind the original SMS dataframe with newly generated  
+##sentiment scores and word counts  
+fin.df<-data.frame(1:nrow(aDf),aDf,fin)
+names(fin.df)<-c("Count",colnames(aDf),"AllWordCount","Score")
 
-#Create date sequence using min and max dates
-mn<-min(as.Date(fin.df$Date))
-mx<-max(as.Date(fin.df$Date))
-date.seq<-data.frame("Date"=seq.Date(from=min(as.Date(fin.df$Date)),to=max(as.Date(fin.df$Date)),by=1))
-date.df<-merge(date.seq,fin.df,by.x="Date",by.y="Date",all.y=T)
+## use df.plot from here on
+df.plot<-fin.df
 
-df.plot<-date.df
-
-# Create Line Chart
-
-#### convert factor to numeric for convenience 
+## convert classes from factors 
 df.plot$Date <- as.Date(df.plot$Date)
-#df.plot$Time <- Will fix and play with time later
+#df.plot$Time <- Will deal  with time later
 df.plot$Type <- as.factor(df.plot$Type)
 df.plot$Num <- as.integer(df.plot$Num)
 df.plot$Name <- as.factor(df.plot$Name)
 df.plot$MSG <- as.character(df.plot$MSG)
-df.plot$AllWordCount
+df.plot$AllWordCount <-as.numeric(df.plot$AllWordCount)
 df.plot$Score <- as.numeric(df.plot$Score) 
 
-nscores <- max(df.plot$Score)
 
-# get the range for the x and y axis 
-xrange <- range(df.plot$Date) 
-yrange <- range(df.plot$circumference) 
 
-# set up the plot 
-plot(xrange, yrange, type="n", xlab="Age (days)",
-     ylab="Circumference (mm)" ) 
-colors <- rainbow(nscores) 
-linetype <- c(1:nscores) 
-plotchar <- seq(18,18+nscores,1)
+#===========PLOTTING===========
 
+
+###PLOT 1) Standard package, using spline
+## range values 
+xrange <- range(1:max(table(df.plot$Type)))
+yrange <- range(df.plot$Score) 
+nnames <- length(levels(df.plot$Type))
+
+## set up plot 
+plot(xrange, yrange, type="n", xlab="Sequential Texts",
+     ylab="Sentiment Score" ) 
+abline(h=0,col=1,lty=2)
+colors <- rainbow(3) 
+linetype <- 1
+plotchar <- seq(18,18+nnames,1)
 # add lines 
-for (i in 1:nscores) { 
-  tree <- subset(df.plot, Tree==i) 
-  lines(tree$age, tree$circumference, type="b", lwd=1.5,
-        lty=linetype[i], col=colors[i], pch=plotchar[i]) 
-} 
+for (p in 1:nnames) { 
+  df.type <- df.plot[df.plot$Type==levels(df.plot$Type)[p],] 
+  scorespline<-spline(df.type$Score)
+  lines(scorespline$x, scorespline$y, lwd=2.0,
+        lty=1, col=colors[p], pch=plotchar[p])
+}
+# add title 
+title("SMS Sentiment")
+# add legend 
+legend(xrange[1], yrange[2], levels(df.plot$Type), cex=0.8, col=colors,
+       pch=plotchar, lty=linetype)
 
-# add a title and subtitle 
-title("Tree Growth", "example of line plot")
 
-# add a legend 
-legend(xrange[1], yrange[2], 1:nscores, cex=0.8, col=colors,
-       pch=plotchar, lty=linetype, title="Tree")
+
+### PLOT 2) Line Chart using ggplot
+
+
+
 
 
